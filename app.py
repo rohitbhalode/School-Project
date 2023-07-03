4596c4185c8023dbc07fe14c3bf28c2b3171aa38
rohitbhalode <134773159+rohitbhalode@users.noreply.github.com>
2023-07-03 19:39:41 +0000

    made some frontend and database changes

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, redirect, session
-from database import login_validation, teacher_info, new_Admission,submit_new_admission
+from flask import Flask, render_template, request, redirect, session, url_for
+from database import login_validation, teacher_info, submit_new_admission, Class_select
 
 app = Flask(__name__)
+app.secret_key = "your_secret_key"
 
 
 @app.route("/")
@@ -9,17 +10,14 @@ def welcome():
   return render_template("home.html")
 
 
-@app.route("/dummy")
-def dummy():
-  return render_template("dummy.html")
-
-
 @app.route("/login")
 def login():
-  return render_template("index.html")
-
-
-Username = None
+  username = session.get('username')
+  if username:
+    # User is already logged in, redirect to teacher_profile
+    return redirect("/teacher_profile")
+  else:
+    return render_template("index.html")
 
 
 @app.route("/submit", methods=['POST'])
@@ -27,18 +25,43 @@ def result():
   Username = request.form.get("Username")
   Password = request.form.get("Password")
   name1 = Username.upper()
-  if (login_validation(Username, Password)):
+
+  if login_validation(Username, Password):
+    session['username'] = Username
     return render_template("teacherlogin.html", name=name1)
   else:
     return redirect("/login")
 
 
-@app.route("/teacher_profile")
+'''
+@app.route("/submit", methods=['POST'])
+def result():
+  Username = request.form.get("Username")
+  Password = request.form.get("Password")
+  name1 = Username.upper()
+  if (login_validation(Username, Password)):
+    session['username'] = Username
+    return render_template("teacherlogin.html", name=name1)
+  else:
+    return redirect("/login")
+
+'''
+
+
+@app.route("/teacher_profile", methods=['GET', 'POST'])
 def teacher_information():
-  print("here is new admission detail i am printing")
-  new_Admission()
-  d = teacher_info("Umesh kumbhar")
-  return render_template("teacherlogin.html", name=Username, detail=d)
+  username = session.get('username')  # Retrieve the username from the session
+  if username:
+    d = teacher_info("Umesh kumbhar")
+    return render_template("teacherlogin.html", name=username, detail=d)
+  else:
+    return redirect("/login")
+
+
+@app.route("/logout")
+def logout():
+  session.clear()
+  return redirect("/login")
 
 
 @app.route("/new_Admission")
@@ -48,15 +71,18 @@ def new_Admission():
 
 @app.route("/submit_admission", methods=['POST'])
 def submit_admission():
-  l=[]
-  l.append(request.form.get("name"))
-  l.appned(request.form.get("Fname"))
-  l.appned(request.form.get("std"))
-  l.append(request.form.get("age"))
-  l.append(request.form.get("email"))
-  l.append(request.form.get("course"))
-  
+  form_elements = list(request.form.values())
+  submit_new_admission(form_elements)
   return redirect("/teacher_profile")
+
+
+@app.route("/class_data/<int:class_number>")
+def class_data(class_number):
+  print(class_number)
+  a = "Class_" + str(class_number)
+  result = Class_select(a)
+  return render_template("class_data.html", data=result)
+
 
 if __name__ == "__main__":
   app.run(debug=True, host='0.0.0.0')
--- database.py
+++ database.py
@@ -3,8 +3,8 @@ import mysql.connector
 try:
   mydb = mysql.connector.connect(
     host="aws.connect.psdb.cloud",
-    user="nth8yfh32jqrgzpfei1m",
-    password="pscale_pw_DWPpEEW0u9dvuH5V8wJlZmos1pqfOeFIoAurrlijNaS")
+    user="bbhh9tfvizntvohsl6qa",
+    password="pscale_pw_W3NeI5EiDIyT4cpNu8wkV74e9uSktsev7k4rn9STRrQ")
   cursor = mydb.cursor()
   print("connection successfully", mydb)
 except Exception as e:
@@ -33,9 +33,26 @@ def teacher_info(teacher):
 
 
 def submit_new_admission(t):
-  query="insert into Student_info(S_Name,F_Name,Class,Course,P_Username,Password) values (%s,%s,%s,%s,%s,%s)"
 
-  cursor.execute(query,t)
+  Class_Admission(t)
+
+  query = "insert into Student_info(S_Name,F_Name,Class,Course,P_Username,Password,Email,AdmittedBy) values (%s,%s,%s,%s,%s,%s,%s,%s)"
+
+  cursor.execute(query, t)
   mydb.commit()
 
 
+def Class_Admission(t):
+  a = "Class_" + t[2]
+  query = "insert into {} (S_Name,Fname, Course,AdmittedBy) values ('{}','{}','{}','{}')".format(
+    a, t[0], t[1], t[-2], t[-1])
+  cursor.execute(query)
+  mydb.commit()
+
+
+def Class_select(a):
+  #a = "Class_" + str(9)
+  query = "select * from {}".format(a)
+  cursor.execute(query)
+  result = cursor.fetchall()
+  return result
